16d7ae20006a4588301bce019018f2c926fdf550
elenafiodorova <fiodorova.elena.25.08@gmail.com>
2020-03-22 16:14:36 +0300

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let minus=0;
+    if (n<0) {
+        minus = 1;
+        n=n*(-1);
+    }
+    let str=String(n);
+    let a=str.split('').reverse().join('');
+    let number=+a;
+    //if (minus=0) {return number}
+      //  else {return number*(-1)}
+      return number;
 }
